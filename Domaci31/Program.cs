7a73b9ba5d338161b7acfae5d3ed39c54bf7a7c7
Naser Ljajic <naserljajic@gmail.com>
2020-06-29 22:30:17 +0200

    Neke sitne izmene

--- Domaci31/Program.cs
+++ Domaci31/Program.cs
@@ -30,14 +30,14 @@ namespace Domaci31
                         {
                             Console.Write("Unesite sifru artikla (Sifra moze biti SAMO celobrojna vrednost): ");
                             int sifraa = int.Parse(Console.ReadLine());
-                            bool provera = false;
+                            bool proveraa = false;
                             for (var i = 0; i < ArtikliSifra.Count; i++)
                                 if (ArtikliSifra[i] == sifraa)
                                 {
                                     Console.WriteLine("Uneta sifra se uklapa sa vec postojecom sifrom!");
-                                    provera = true;
+                                    proveraa = true;
                                 }
-                            if (provera)
+                            if (proveraa)
                                 continue;
                             ArtikliSifra.Add(sifraa);
                             Console.Write("Unesite naziv artikala: ");
@@ -52,6 +52,7 @@ namespace Domaci31
                             ArtikliCena.Add(float.Parse(Console.ReadLine()));
                             Console.Write("Unesite kolicinu artikla: ");
                             ArtikalKolicina.Add(int.Parse(Console.ReadLine()));
+                            Console.WriteLine("USPESNO STE DODALI ARTIKAL!");
                             break;
                         }
 
@@ -65,42 +66,152 @@ namespace Domaci31
                         }
                         else
                         {
-                            Console.WriteLine("Sifra artikla, Naziv , Cena, Kolicina");
+                            Console.WriteLine("Sifra artikla, Naziv, Cena, Kolicina");
                             for (int i = 0; i < ArtikliSifra.Count; i++)
                                 Console.WriteLine($"{ArtikliSifra[i]}, {ArtikliNaziv[i]}, {ArtikliCena[i]}, {ArtikalKolicina[i]}");
                             break;
                         }
                     case "3":
-                        Console.WriteLine("------------Izmeni Artikal------------");
+                        Console.WriteLine("----------------IZMENITE ARTIKAL-----------------");
+                        bool proveraaa = false;
+                        int sifra =0;
+                        int index =0;
+                        int brojac =0;
+                        int sifraaa = 0;
                         while (true)
                         {
-                            Console.Write("Unesite sifru artikla: ");
-                            int sifra = int.Parse(Console.ReadLine());
-                            int brojac = 0;// brojac je potreban da bi videli da li postoji artikal sa unetom sifrom.
-                            int index = 0;// index nam je potreban da bi sacuvao index liste.
+                            if (ArtikliSifra.Count == 0)
+                            {
+                                Console.WriteLine("Nije dodat niti jedan artikal!");
+                                break;
+                            }
+                            Console.WriteLine(" ");
+
+                            if (!proveraaa)
+                            {
+                                Console.Write("Unesite sifru artikla: ");
+                                sifra = int.Parse(Console.ReadLine());
+
+                            }
+                            else
+                            {
+                                sifra = sifraaa;
+                                brojac = 0;
+
+                            }
+                                
+
                             for (int i = 0; i < ArtikliSifra.Count; i++)
                                 if (sifra == ArtikliSifra[i])
                                 {
                                     brojac++;
                                     index = i;
+                                    sifraaa = sifra;
                                 }
                             if (brojac == 1)
                             {
+                                Console.WriteLine("Sifra artikla, Naziv, Cena, Kolicina");
                                 Console.WriteLine($"{ArtikliSifra[index]}, {ArtikliNaziv[index]}, {ArtikliCena[index]}, {ArtikalKolicina[index]}");
-                                Console.Write("Unesite novo stanje kolicine datog artikla: ");
-                                ArtikalKolicina[index] = int.Parse(Console.ReadLine());
-                                Console.WriteLine("USPESNO STE IZMENILI KOLICINU ARTIKLA!");
-                                break;
+                                Console.WriteLine("Odaberite opciju 1. Izmenite sifru, 2. Izmenite naziv, 3. Izmenite cenu, 4. Izmenite kolicinu, 5. Obrisi artikal, 6. Vrati se na pocetni meni");
+                                string unoss = Console.ReadKey().KeyChar.ToString();
+                                Console.WriteLine(" ");
+                                switch (unoss)
+                                {
+                                    case "1":
+                                        {
+                                            while (true)
+                                            {
+                                                Console.Write("Unesite novu sifru artikla: ");
+                                                int sifraa = int.Parse(Console.ReadLine());
+                                                bool provera = false;
+                                                for (var i = 0; i < ArtikliSifra.Count; i++)
+                                                    if (ArtikliSifra[i] == sifraa)
+                                                    {
+                                                        Console.WriteLine("NIJE MOGUCE DODELITI UNETU SIFRU JER JE VEC DODELJENA NEKOM ARTIKLU!");
+                                                        provera = true;
+                                                    }
+                                                if (provera)
+                                                    continue;
+                                                ArtikliSifra[index] = sifraa;
+                                                sifraaa = sifraa;
+                                                Console.WriteLine("USPESNO JE IZMENJENA SIFRA ARTIKLA!");
+                                                break;
+                                            }
+
+                                            break;
+                                        }
+                                    case "2":
+                                        {
+                                            Console.Write("Unesite novi naziv artikla: ");
+                                            ArtikliNaziv[index] = Console.ReadLine();
+                                            Console.WriteLine("USPESNO JE IZMENJEN NAZIV ARTIKLA!");
+                                            break;
+                                        }
+                                    case "3":
+                                        {
+                                            Console.Write("Unesite novu cenu artikla: ");
+                                            ArtikliCena[index] = int.Parse(Console.ReadLine());
+                                            Console.WriteLine("USPESNO JE IZMENJENA CENA ARTIKLA!");
+                                            break;
+                                        }
+                                    case "4":
+                                        {
+                                            Console.Write("Unesite novo stanje kolicine datog artikla: ");
+                                            ArtikalKolicina[index] = int.Parse(Console.ReadLine());
+                                            Console.WriteLine("USPESNO STE IZMENILI KOLICINU ARTIKLA!");
+                                            break;
+
+                                        }
+                                    case "5":
+                                        {
+                                            Console.Write("Da li ste sigurni da zelite da obrisete artikal? 1. DA, 2. NE: ");
+                                            String izbor = Console.ReadKey().KeyChar.ToString();
+                                            switch (izbor)
+                                            {
+                                                case "1":
+                                                    {
+                                                        ArtikliSifra.RemoveAt(index);
+                                                        ArtikliNaziv.RemoveAt(index);
+                                                        ArtikliCena.RemoveAt(index);
+                                                        ArtikalKolicina.RemoveAt(index);
+                                                        Console.WriteLine("Uspesno ste obrisali artikal!");
+                                                        break;
+                                                    }
+                                                case "2":
+                                                    break;
+                                            }
+
+                                            break;
+                                        }
+                                    case "6":
+                                        break;
+                                    default:
+                                        break;
+                                }
+                                if (unoss == "6")
+                                    break;
+                                Console.Write("Da li zelite da nastavite sa izmenom artikla? 1. DA, 2. NE (Vrati se na pocetni meni)");
+                                Console.WriteLine(" ");
+                                String izborr = Console.ReadKey().KeyChar.ToString();
+                                if (izborr == "1")
+                                {
+                                    proveraaa = true;
+                                    continue;
+                                }
+                                    
+                                else
+                                {
+                                    break;
+                                    
+                                }
                             }
                             else
                             {
                                 Console.WriteLine("Uneta sifra artikla se ne poklapa sa siframa u magacinu!");
                                 continue;
                             }
-
                         }
                         break;
-                        
                     case "4":
                         break;
                     default:
